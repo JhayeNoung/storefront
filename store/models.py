20d0b094a06125b3120d83a4446553493f41034b
James <james.noung@gmail.com>
2024-10-07 21:21:43 +0700

    first commit

---
+++ store/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+# Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    # don't need to add products = models.ManyToManyField(Product), one class is OK. Django will automatically create reverse relationship
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+     
+class Product(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'BRONZE'),
+        (MEMBERSHIP_SILVER, 'SILVER'),
+        (MEMBERSHIP_GOLD, 'GOLD'),
+    ]
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField() # slug makes search engine more see to the title
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True) # set date every time the object is saved
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT) # if we delete our collection, product shouldn't be deleted
+    promotions = models.ManyToManyField(Promotion) # 
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True) # we use DateField because we don't care the time of birth
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'PENDING'),
+        (PAYMENT_STATUS_COMPLETE, 'COMPLETE'),
+        (PAYMENT_STATUS_FAILED, 'FALIED'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True) # set date at the time of object is created
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT) # if the customer is deleted, we shouldn't delete the order because order represents our sales
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField() # positive small interger field prevents giving negative value
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2) # product already has price, but price can change, so it should have the price at the time of order
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE) # if the cart is deleted, all the cartitem is deleted
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
