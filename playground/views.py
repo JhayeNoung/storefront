4456c5c1ed85c142b22263afbee152f06419c807
James <james.noung@gmail.com>
2024-10-08 05:54:12 +0700

    add say_hello url`
    "

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+def say_hello(request):
+    Product = Product.objects.all()
+    return render(request, 'hello.html', {'name': 'jhaye'})
