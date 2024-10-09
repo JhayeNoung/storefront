0f500a7d7efe0f2cda400b1e263d18822bd6bdaf
James <james.noung@gmail.com>
2024-10-09 08:49:00 +0700

    add more notes about values

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from store.models import Product, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.filter(id__in=OrderItem.objects.values('product__id')).order_by('title')
+    query_set = Product.objects.values('id', 'title', 'collection__title').filter(collection__id=1).order_by('title')[:5]
     product_obj = Product.objects.get(pk=1)
     # product_obj = Product.objects.all()
     return render(request, 'hello.html', {'name': 'jhaye', 'products':list(query_set), 'product_obj':product_obj})
