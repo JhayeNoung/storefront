b6d83e1a9025991470b88f1639b9c3315a5fbd4e
James <james.noung@gmail.com>
2024-10-13 14:18:37 +0700

    adding get_tags_for to TaggedItem.objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.db.models import F, Count, Value, Func
 from store.models import Product, OrderItem, Order, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem, TaggedItemManager
+
 
 # Create your views here.
 def say_hello(request):
-    query_set = Customer.objects.annotate(orders_count=Count('order'))
+    query_set = TaggedItem.objects.get_tags_for(Product, 1)
     # product_obj = Product.objects.prefetch_related('promotions')
     return render(request, 'hello.html', {'name': 'jhaye', 'query_set':query_set})
